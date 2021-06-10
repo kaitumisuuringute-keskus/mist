de61f08313952d68c875ed6ac789a690c17eba8d
Souly <soulyyy@gmail.com>
2021-06-10 10:34:05 +0300

    no need for the runs, this is a bash script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,6 @@ cd ${MIST_HOME}
 
 MASTER_ID=$(cat /proc/1/cgroup | grep "/docker/" | head -n 1 | awk -F "/" '{print $NF}')
 export MIST_OPTS="$MIST_OPTS -Dmist.workers.docker.auto-master-network.container-id=$MASTER_ID"
-run ls -la ./bin/
-run ls -la
+ls -la ./bin/
+ls -la
 exec ./bin/mist-master start --debug true $@
